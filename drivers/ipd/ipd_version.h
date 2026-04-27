4a688f12531e5e3b7cc2d8606df573c021553c8e
Keshav Gupta <keshavg@marvell.com>
2026-04-27 14:05:28 +0530

    Updating IPD driver version to release T7.0.0.1.0.1

--- drivers/ipd/ipd_version.h
+++ drivers/ipd/ipd_version.h
@@ -36,7 +36,7 @@ disclaimer.
  * Current IFCS IPD version of the release
  * Version: Major, Minor, SubMinor, Branch, Maintenance and Debug
  */
-#define IPD_VERSION_STRING "0.16.4.0.0.0"
+#define IPD_VERSION_STRING "0.16.4.2.0.1"
 static char *ipd_version = IPD_VERSION_STRING;
 
 #endif // __IPD_VERSION_H__
